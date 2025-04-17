0035fc76e6deb473bf3e8dcb6f1ca85448528ed3
a3o <>
2025-04-17 10:40:54 +0200

    added border to blink

--- init.lua
+++ init.lua
@@ -991,9 +991,10 @@ require('lazy').setup({
       },
 
       completion = {
+        menu = { border = 'single' },
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
       },
 
       sources = {
@@ -1012,10 +1013,10 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = true, window = { border = 'single' } },
     },
   },
 
